61d21ad61a887099d306e83c0d353fd4ff9fab16
Nicholas Clawson <nickclaw@gmail.com>
2014-10-18 01:33:22 -0700

    Added number guessing game

---
+++ compute/src/games/numberwang.js
@@ -0,0 +1,40 @@
+var _ = require('lodash'),
+    Log = require('../Logger.js');
+
+module.exports = Numberwang;
+
+function Numberwang() {
+    this.number = Math.floor(Math.random() * 100);
+    this.finished = false;
+    this.lastGuess = null;
+
+    this.maxPlayers = 1;
+    this.minPlayers = 1;
+}
+
+_.extend(Numberwang.prototype, {
+
+    /**
+     * Updates a players move
+     * @param {Object} move
+     * @param {Socket} user
+     * @param {Function} callback - node style
+     */
+    update: function(move, user, callback) {
+        Log.info('user ' + user.id + ' moved', move, '-', this.number);
+
+        this.finished = this.number === move.guess;
+        this.lastGuess = move.guess;
+
+        callback(null, this.getState());
+    },
+
+    /**
+     * Returns the current state of the game
+     */
+    getState: function() {
+        if (this.finished) return 'You won!';
+        if (this.lastGuess > this.number) return 'Too big!';
+        return 'Too small!';
+    }
+});
